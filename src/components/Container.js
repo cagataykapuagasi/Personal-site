c69101f26c67a3724f9241b03d013fd9dea3ff89
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-10 15:36:02 +0300

    Add get props

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ import React from 'react';
 export default class Container extends React.Component {
   render() {
     return (
-      <div>
+      <div {...this.props}>
         {this.props.children}
       </div>
     )
