0491e2654b6778f9ffa2a2e3263cfe042c12c654
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-10 15:36:15 +0300

    index.js

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Icons from './Icons';
 import Container from './Container';
+import InPopup from './Popup';
+import Button from './Button';
 
 export {
   Icons,
   Container,
+  InPopup,
+  Button,
 }
