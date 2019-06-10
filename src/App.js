15935c70f261ede120286854dd76020c4511ef52
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-10 15:35:37 +0300

    load datas from store

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Splash, Blog } from './pages';
 import history from './history';
 
+
 export default class App extends React.Component {
+
+  componentDidMount() {
+    root.blog.loadDatas();
+  }
 
   render() {
     return (
