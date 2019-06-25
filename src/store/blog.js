879712a527ed57ed35640ec817d8a3c854a53b61
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-25 22:03:50 +0300

    get data and fill currentData

--- src/store/blog.js
+++ src/store/blog.js
@@ -3,62 +3,81 @@ import { Config } from '../FirebaseConfig/config';
 import firebase from 'firebase';
 
 
-
 const db = firebase.initializeApp(Config);//start a firebase application
 const doc = db.database().ref().child('data');//Database
 
 export default class BlogStore {
 
-  datas;
+  currentData;
+
+  allData = [];
+
+  buttonText = 'Login';
 
   constructor(rootStore) {
     this.rootStore = rootStore;
-
   }
+
 
   loadDatas = async () => {
+
     await doc.once('value', (snap) => {
-      this.datas = snap.val();
+      let temp = Object.values(snap.val());
+      this.currentData = temp;
+      this.allData[0] = temp;
+      this.allData[1] = temp.filter(({field}) => field === 'React');
+      this.allData[2] = temp.filter(({field}) => field === 'React Native');
+      this.allData[3] = temp.filter(({field}) => field === 'C#');
+      this.allData[4] = temp.filter(({field}) => field === 'C');
+      this.allData[5] = temp.filter(({field}) => field === 'Database');
     })
-    //this.addPost('test3', 'test3 text', this.getTime());
   }
 
-  loginAdmin = (username, password) => {
+  changeCurrentData = (index) => {
+    this.currentData = this.allData[index];
+  }
+
+  loginAdmin = (username, password, history) => {
     firebase.auth().signInWithEmailAndPassword(username, password)
-    .then(() => console.log('açtın sus'))
+      .then(() => {
+        history.push('/admin');
+      })
+      .catch((e) => {
+        if (e.code === 'auth/network-request-failed') {
+          alert('Check your connection');
+        } else {
+          alert('Just administrator');
+        }
+      })
+      .finally(() => {
+        this.buttonText = 'Login';
+      })
   }
 
-  addPost = async (title, text, time) => {
-    await doc.push().set({
-      title,
-      text,
-      time,
-    })
-    await doc.orderByChild('time').equalTo(time).once('value', (snap) => {
+  addPost = async (data, history) => {
+
+    await doc.push().set(
+      data
+    )
+    await doc.orderByChild('time').equalTo(data.time).once('value', (snap) => {
       db.database().ref('data/' + Object.keys(snap.val())[0]).update({
         id: Object.keys(snap.val())[0]
       })
+    }).then( async () => {
+      await this.loadDatas();
+      history.push('/blog')
     })
   }
-
-  getTime = () => {
-    const time = new Date();
-    const hours = time.getHours();
-    const minutes = time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes();
-    const day = time.getDate() < 10 ? '0' + time.getDate() : time.getDate();
-    const month = time.getMonth() + 1 < 10 ? '0' + time.getMonth() : time.getMonth();
-    const year = time.getFullYear();
-
-    return hours + ':' + minutes + ' ' + day + '/' + month + '/' + year;
-  }
-
 
 
 }
 
 decorate(BlogStore, {
   loadDatas: action,
-  datas: observable,
+  currentData: observable,
   addPost: action,
   loginAdmin: action,
+  buttonText: observable,
+  allData: observable,
+  changeCurrentData: action,
 });
