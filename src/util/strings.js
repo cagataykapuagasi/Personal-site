d0bec45379eb0c397eaabd0f08bccdb041adfc4e
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-25 22:04:02 +0300

    move string to util

---
+++ src/util/strings.js
@@ -0,0 +1,17 @@
+export const data = [
+  "Dashboard",
+  "React",
+  "React Native",
+  "C#",
+  "C",
+  "Database"
+]
+
+export const photoUri = "https://scontent.fbtz1-10.fna.fbcdn.net/v/t1.0-9/14572352_10207206617059789_2356825048072629179_n.jpg?_nc_cat=110&_nc_ht=scontent.fbtz1-10.fna&oh=6bef784f4e999e0363a17a11fc3e5081&oe=5D7CC78F";
+
+
+
+export default {
+  data,
+  photoUri,
+}
