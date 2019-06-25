1576abf71ab71380b7316b6ff7c6b729ee7c21ae
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-25 21:58:49 +0300

    new components

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,16 @@ import Icons from './Icons';
 import Container from './Container';
 import InPopup from './Popup';
 import Button from './Button';
+import { MenuList } from './Menu';
+import { PostList } from './Post';
+import MyLoader from './MyLoader';
 
 export {
   Icons,
   Container,
   InPopup,
   Button,
+  MenuList,
+  PostList,
+  MyLoader,
 }
