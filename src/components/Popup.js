efefbd2926e6ad17add02be7220c9d79cf559876
Cagatay Kapuagasi <cagatay.kapuagasi@gmail.com>
2019-06-25 22:01:13 +0300

    add new design etc

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -2,13 +2,16 @@ import React from 'react';
 import { Container, Button } from './';
 import { observer, inject } from 'mobx-react';
 import { StyleSheet, css } from 'aphrodite';
+import { photoUri } from '../util/strings';
+
+
 
 class InPopup extends React.Component {
 
   state = {
     username: '',
     password: '',
-    borderStyle: '#F5515F'
+    borderStyle: '#F5515F',
   }
 
   onMouseLeave = () => {
@@ -20,22 +23,27 @@ class InPopup extends React.Component {
   }
 
   loginClick = () => {
-    const { blog } = this.props.root;
     const { password, username } = this.state;
-    blog.loginAdmin(username, password);
+    const { blog } = this.props.root;
+    const { history } = this.props;
+
+    blog.buttonText = 'Loading..';
+    blog.loginAdmin(username, password, history);
   }
 
   render() {
     const { borderStyle, password, username } = this.state;
+    const { buttonText } = this.props.root.blog;
     
     return (
       <Container className={css(styles.container)}>
-        <p className={css(styles.text)}>Only Admin</p>
-        <input className={css(styles.input)} value={username} placeholder="username" onChange={(event) => this.setState({ username: event.target.value })} />
-        <input className={css(styles.input)} value={password} placeholder="password" onChange={(event) => this.setState({ password: event.target.value })} />
-        <Button borderColor={borderStyle} onClick={this.loginClick} text="Login" style={styles.button} textStyle={styles.buttonText}
+        <img className={css(styles.image)} src={photoUri} />
+        <input className={css(styles.input)} value={username} placeholder="Email" onChange={(event) => this.setState({ username: event.target.value })} />
+        <input type='password' className={css(styles.input)} value={password} placeholder="Password" onChange={(event) => this.setState({ password: event.target.value })} />
+        <Button borderColor={borderStyle} onClick={this.loginClick} text={buttonText}
           onMouseLeave={this.onMouseLeave} onMouseOver={this.onMouseOver}
         />
+        
       </Container>
     )
   }
@@ -47,27 +55,27 @@ const styles = StyleSheet.create({
   container: {
     height: 500,
     width: 400,
-    backgroundColor: '#1d1d1d',
-    borderRadius: 100,
+    backgroundColor: '#161a20',
+    borderRadius: 5,
     justifyContent: 'center',
     alignItems: 'center',
     display: 'flex',
     flexDirection: 'column',
   },
   input: {
-    height: 20,
+    height: 25,
     width: 200,
     margin: 10,
-    borderRadius: 5
+    borderRadius: 5,
+    borderColor: 'gray',
+    backgroundColor: '#d4d4d4',
+    borderWidth: 4
   },
-  text: {
-    fontSize: 60,
-    fontFamily: "AvenirNext-UltraLight",
-    webkitTouchCallout: 'none',
-    webkitUserSelect: 'none',
-    khtmlUserSelect: 'none',
-    mozUserSelect: 'none',
-    msUserSelect: 'none',
-    userSelect: 'none',
-  },
+  image: {
+    height: 90,
+    width: 90,
+    marginBottom: 30,
+    position: 'relative',
+    borderRadius: 45
+  }
 })
